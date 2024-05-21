92f7482bb178dd6bdbed7ac7f80cc1e5ece60414
MinhajurRahmanCIS <rahamannovel@gmail.com>
2024-05-22 04:28:47 +0600

    teacher can remove student from their class

--- src/Pages/ClassInfo/ClassPeople/ClassPeople.js
+++ src/Pages/ClassInfo/ClassPeople/ClassPeople.js
@@ -1,10 +1,68 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
+import PeopleList from './PeopleList';
 const ClassPeople = () => {
     const { id } = useParams();
+    const { data: peoples = [], isLoading, refetch } = useQuery({
+        queryKey: ["peoples", id],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/enrollmentPeople?classId=${id}`, {
+                headers: {
+                    authorization: `bearer ${localStorage.getItem("quickEdu-token")}`
+                }
+            });
+            const data = await res.json();
+            return data;
+        }
+    });
+
+    if (isLoading) {
+        return <Loading></Loading>
+    };
+
     return (
         <div>
-            Under Development
+            {
+                peoples?.data?.length > 0 ?
+                    <>
+                        <div className="flex flex-col w-full">
+                            <div className="divider divider-end font-semibold">{peoples?.data.length} Students </div>
+                        </div>
+                        <div className="overflow-x-auto border">
+                            <table className="table text-center text-xl">
+                                {/* head */}
+                                <thead>
+                                    <tr className="text-black text-xl">
+                                        <th>No</th>
+                                        <th></th>
+                                        <th>Email</th>
+                                        <th>Action</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        peoples &&
+                                        peoples?.data.map((people, i) =>
+                                            <PeopleList
+                                                key={people._id}
+                                                people={people}
+                                                i={i}
+                                                refetch={refetch}
+                                            >
+                                            </PeopleList>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
+                    :
+                    <div className="flex justify-center items-center text-xl p-10">
+                        <h1>No student is enrolled!</h1>
+                    </div>
+            }
         </div>
     );
 };
---
+++ src/Pages/ClassInfo/ClassPeople/PeopleList.js
@@ -0,0 +1,85 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider';
+import useTeacher from '../../../hooks/useTeacher';
+import Loading from '../../Shared/Loading/Loading';
+import Swal from 'sweetalert2';
+
+const PeopleList = ({ people, i, refetch }) => {
+    const { user } = useContext(AuthContext);
+    const [isTeacher, isTeacherLoading] = useTeacher(user?.email)
+    const { image, name, email } = people;
+
+    const handelDeletePeople = email => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this Student!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Remove Student"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/enrollmentPeople/${email}`, {
+                    method: "DELETE",
+                    headers: {
+                        authorization: `bearer ${localStorage.getItem("quickEdu-token")}`
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.success) {
+                            refetch();
+                            Swal.fire({
+                                title: "Student Removed!",
+                                text: "Your Student has been Removed.",
+                                icon: "success"
+                            });
+                        }
+                        else {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Oops...",
+                                text: "Something went wrong! Please Try Again"
+                            });
+                        }
+                    })
+            };
+        });
+    };
+
+    if (isTeacherLoading) {
+        return <Loading></Loading>
+    };
+
+    return (
+        <tr className="hover">
+            <th>
+                {i + 1}
+            </th>
+            <td>
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                            <img src={image} alt='' />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{name}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {email}
+            </td>
+            <td>
+                {
+                    isTeacher &&
+                    <button onClick={() => handelDeletePeople(email)} className="btn btn-error">Remove</button>
+                }
+            </td>
+        </tr>
+    );
+};
+
+export default PeopleList;
